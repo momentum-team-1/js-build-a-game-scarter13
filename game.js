f2ac5843794637e12c8568065f778bee6095f5be
scarter13 <scarter13@gmail.com>
2020-05-13 21:58:22 -0400

    first enemy drawn, parked and not moving

--- game.js
+++ game.js
@@ -7,10 +7,12 @@ class Game {
         let gameSize = { x: canvas.width, y: canvas.height}
         this.player = new Player (gameSize)
         this.coin = new Coin (gameSize)
+        this.enemy = new Enemy
         let animate = () => {
             this.update()
             this.drawPlayer(screen, gameSize)
             this.drawCoin(screen)
+            this.drawEnemy(screen)
             requestAnimationFrame(animate)
         }
         animate ()
@@ -38,11 +40,16 @@ class Game {
         let coinWidth = this.coin.size.x
         let coinHeight = this.coin.size.y
         screen.fillRect (coinXPosition, coinYPosition, coinWidth, coinHeight)
-
-
-
     }
-    
+    drawEnemy(screen) {
+        console.log ("drawing enemy")
+        screen.fillStyle="black"
+        let enemyXPosition = this.enemy.center.x - this.enemy.size.x / 2
+        let enemyYPosition = this.enemy.center.y - this.enemy.size.y / 2
+        let enemyWidth = this.enemy.size.x
+        let enemyHeight = this.enemy.size.y
+        screen.fillRect (enemyXPosition, enemyYPosition, enemyWidth, enemyHeight)
+    }
     update () {
         this.player.update()
         this.player.limit()
@@ -50,7 +57,7 @@ class Game {
             this.player.center.x - this.player.size.x/2 > this.coin.center.x + this.coin.size.x/2 ||
             this.player.center.y + this.player.size.x/2 < this.coin.center.y - this.coin.size.x/2 ||
             this.player.center.y - this.player.size.x/2 > this.coin.center.y + this.coin.size.x/2){
-                console.log("no collision")
+                // console.log("no collision")
             }  else {
                 this.coin = new Coin 
             }
@@ -102,20 +109,22 @@ class Coin {
             if (ranX === 0) {this.center.x = 175}
             else if (ranX === 1) {this.center.x = 225}
             else if (ranX === 2) {this.center.x = 275}
-            console.log (this.center.x)
+            // console.log (this.center.x)
         let ranY = (Math.floor(Math.random() * 3))
             if (ranY === 0) {this.center.y = 175}
             else if (ranY === 1) {this.center.y = 225}
             else if (ranY === 2) {this.center.y = 275}
 
-
-
-
-
     }
 
 
+}
 
+class Enemy {
+    constructor (){
+    this.size = {x: 30, y: 30}
+    this.center = {x: 175, y: 0}
+    }
 }
 
 new Game ()
